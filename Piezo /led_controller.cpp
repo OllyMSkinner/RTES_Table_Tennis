eb43eb340bbcfa45d86c1df0cd0690470e602634
OllyMSkinner <39485815+OllyMSkinner@users.noreply.github.com>
2026-04-13 19:15:41 +0100

    Refactor LEDController to remove red LED functionality
    Removed red LED control methods and related logic.

--- Piezo /led_controller.cpp
+++ Piezo /led_controller.cpp
@@ -4,20 +4,13 @@
 #include <string>
 
 LEDController::LEDController(LEDControllerSettings settings)
-    : redGpio_(settings.redGpio),
-      greenGpio_(settings.greenGpio)
+    : greenGpio_(settings.greenGpio)
 {
     const std::string chipPath = "/dev/gpiochip" + std::to_string(settings.chipNumber);
 
     chip_ = std::make_shared<gpiod::chip>(chipPath);
 
     gpiod::line_config line_cfg;
-
-    line_cfg.add_line_settings(
-        redGpio_,
-        gpiod::line_settings()
-            .set_direction(gpiod::line::direction::OUTPUT)
-            .set_output_value(gpiod::line::value::INACTIVE));
 
     line_cfg.add_line_settings(
         greenGpio_,
@@ -47,18 +40,6 @@ LEDController::~LEDController()
     }
 }
 
-void LEDController::redOn()
-{
-    request_->set_value(redGpio_, gpiod::line::value::ACTIVE);
-    redActive_ = true;
-}
-
-void LEDController::redOff()
-{
-    request_->set_value(redGpio_, gpiod::line::value::INACTIVE);
-    redActive_ = false;
-}
-
 void LEDController::greenOn()
 {
     request_->set_value(greenGpio_, gpiod::line::value::ACTIVE);
@@ -73,14 +54,7 @@ void LEDController::greenOff()
 
 void LEDController::allOff()
 {
-    redOff();
     greenOff();
-}
-
-void LEDController::flashRed(int flashMs)
-{
-    redOn();
-    redOffTime_ = std::chrono::steady_clock::now() + std::chrono::milliseconds(flashMs);
 }
 
 void LEDController::flashGreen(int flashMs)
@@ -97,11 +71,6 @@ void LEDController::service()
 void LEDController::updateOutputs()
 {
     const auto now = std::chrono::steady_clock::now();
-
-    if (redActive_ && now >= redOffTime_)
-    {
-        redOff();
-    }
 
     if (greenActive_ && now >= greenOffTime_)
     {
